8a4d7b172c3b1d7b7dad6251f3fa428527a6a2f3
Jakub Sliacan <jsliacan@redhat.com>
2024-03-16 23:49:19 +0100

    Add modularity events detection method
    data/ot_event.csv are filtered press_length>10 events with some
    description values

--- __main__.py
+++ __main__.py
@@ -6,7 +6,128 @@ Script utilizing code in overtaking package
 #! /usr/bin/python3
 
 
+# --------- modularity ----------
 
+import os
+import matplotlib.pyplot as plt
+import networkx as nx
+from src import box, constants, util
+from src import modularity as mod
+
+dflist = util.get_box_files(constants.DATA_HOME)
+util.ensure_date_in_filenames(dflist)
+dflist = util.get_box_files(constants.DATA_HOME)
+
+ot_events = []
+
+for csv_file in dflist:
+    print(csv_file, flush=True)
+    csvr = util.read_csv(csv_file)
+    ldata = box.make_ldata(csvr)  # CSV data as a list
+    # presses
+    press_starts, press_lengths = box.get_press_lengths_and_starts(ldata)
+    date_string = csv_file.split("/")[-1][:8]
+    
+    b_partitions, a_partitions, b_modularities, a_modularities = mod.get_partitions(ldata, press_starts, press_lengths)
+    
+    for j, b_parts in enumerate(b_partitions):
+        
+        if b_modularities[j] == -1: # it's an oncoming case
+            continue
+
+        ps = press_starts[j]
+        pmod = b_modularities[j]
+        part_and_size_pairs = [(len(x), x) for x in b_parts]
+        part_and_size_pairs.sort(reverse=True) # sorts pairs by 1st item, i.e. size
+        press_gap = sum([s for s, p in part_and_size_pairs])
+        lat_dists = [ldata[ps-x][4] for x in range(press_gap)]
+        dispersion_score = mod.dispersion_score(lat_dists)
+                     
+        plt.scatter(range(len(lat_dists)), lat_dists, c='b')
+
+        # ---- process the partition -----
+        for s, p in part_and_size_pairs:
+
+            lp = list(p)
+            lp.sort() # should already be sorted
+            
+            # only take 1st part of a partition if there's a big gap in time
+            # TODO: append the latter part back to partitions, and adjust part_and_size_pairs.
+            for i in range(1,s):
+                if lp[i]-lp[i-1] > 10:
+                    s = i
+                    lp = lp[:s]
+                    break
+
+            # skip the maxed-out readings
+            if min([lat_dists[x] for x in p]) > 500:
+                continue
+            
+            # skip the readings stuck too low
+            if max([lat_dists[x] for x in p]) < 50: 
+                continue
+            
+            # not a real part
+            if s < 3: 
+                continue
+                
+            # --- max clique method ----
+            G = nx.Graph()
+            G.add_nodes_from(lp)
+
+            G.add_edges_from([(lp[x],lp[y]) for x in range(s-1) for y in range(x+1, s) if abs(lat_dists[lp[x]]-lat_dists[lp[y]])<40])
+            mc = nx.approximation.max_clique(G)
+            additional_vertices = set()
+            for v in mc:
+                v_ball = [lat_dists[v]-30, lat_dists[v]+30]
+                for u in G.nodes:
+                    if u in mc:
+                        continue
+                    # make sure the vertex we include with the clique doesn't have a very small degree
+                    if lat_dists[u] > v_ball[0] and lat_dists[u] < v_ball[1] and G.degree(u) > 0.7*len(mc):
+                        additional_vertices.add(u)
+                
+            ot = list(mc)+list(additional_vertices)
+            ot_event = [date_string, ps, dispersion_score, pmod, len(ot), ot]
+            ot_events.append(ot_event)
+            
+            plt.scatter(ot, [lat_dists[x] for x in ot], c='r')
+            
+            # --- end of max clique ----
+
+            """
+            # --- re-run louvain method ----
+            G = nx.Graph()
+            G.add_nodes_from(list(p))
+            lp = list(p)
+            G.add_edges_from([(lp[x],lp[y]) for x in range(s-1) for y in range(x+1, s) if abs(lat_dists[lp[x]]-lat_dists[lp[y]])<40])
+            if len(G.edges) == 0:
+                plt.scatter(lp, [lat_dists[x] for x in lp], c='r')
+                continue
+            # resolution <1 favors bigger parts
+            # resolution >1 favors smaller parts
+            fine_partition = nx.community.louvain_communities(G, resolution=1)
+            fine_pairs = [(len(x), x) for x in fine_partition]
+            fine_pairs.sort()
+            ot = fine_pairs.pop()
+            plt.scatter(list(ot[1]), [lat_dists[x] for x in ot[1]], c='r')
+
+            # --- end of re-run louvain ----
+
+
+            # --- do nothing else method ---
+            plt.scatter(list(p), [lat_dists[x] for x in p], c='r')
+            # --- end of do nothing else ---
+            """
+            
+            break # after getting to the OT event
+            
+        plt.ylim([0,700])
+        plt.savefig(os.path.join("out", "mod", date_string+"_ld_"+str(press_starts[j])+"_"+"{:.6f}".format(pmod)+"_disp="+str(dispersion_score)+"_clique.png"))
+        plt.clf()
+
+util.write_to_csv_file(os.path.join("data", "ot_events.csv"), ot_events)
+        
 """
 # --------- vehicle recognition from video ----------
 
@@ -56,7 +177,7 @@ df = pd.concat(lst)
 df.to_csv(os.path.join(OUT_FOLDER,"predicted_labels.csv"), index=False)
 os.rename(os.path.join(OUT_FOLDER,"predicted_labels.csv"), os.path.join("data", "predicted_labels.csv"))
 """
-
+"""
 # --------------- solve 1-entry overtakes --------------
 import os
 from src import box, util
@@ -76,7 +197,7 @@ for e in ot_events:
         
 #print("OT events of length 1:", len(singular_ot_events))
 #print("OT events with non-zero flag:", len([e for e in ot_events if e[1] > 0]))
-
+"""
 """
 # --------------- code related to the box --------------
 from collections import Counter
---
+++ src/modularity.py
@@ -0,0 +1,134 @@
+import networkx as nx
+import numpy as np
+
+def run_louvain(nodes):
+    """
+    Nodes: lateral distances (list of length n)
+
+    Ignore resolution param of louvain.
+
+    Graph G = (V,E) and for x,y from V, xy is in E iff (|x-y|<40
+    and). Note that 60 is a bit less than 3s given 22 readings per
+    second.
+    """
+    
+    communities = []
+    modularity = 0
+    
+    n = len(nodes)
+    edges = [(i,j) for i in range(n-1) for j in range(i+1, n)]# if abs(bnodes[i]-bnodes[j])<40] # and j-i<60
+
+    G = nx.Graph()
+    G.add_nodes_from(range(n))
+    G.add_edges_from(edges)
+            
+    G = nx.Graph()
+    G.add_nodes_from(range(n))
+    for i in range(n-1):
+        for j in range(i+1,n):
+            d = abs(nodes[i]-nodes[j])
+            if d < 40: # weight edge at 1 if nodes within 40cm of each other
+                w = 1
+            else:
+                w = 0
+            #else:
+                # w = 1
+                # w = 1/np.sqrt(d)
+                # w = 1/d
+                # w = 1-(1/np.sqrt(40-d))
+                        
+            G.add_edge(i,j, weight=w)
+                
+    if len(edges) == 0:
+        print("Error: graph G has 0 edges. Cannot construct a valid partition.")
+        communities = []
+        modularity = 0
+    else:
+        communities = nx.community.louvain_communities(G)
+        modularity = nx.community.modularity(G, communities)
+
+    return (communities, modularity)
+
+    
+    
+def get_partitions(ldata, press_starts, press_lengths):
+    """
+    Partion the list of lateral distances into parts that contain
+    values which are pairwise close.
+
+    For each press, work out before-press "events" and after-press
+    "events".
+    
+    """
+    bcomms = []
+    bmod = []
+    acomms = []
+    amod = []
+
+    for pi, ps in enumerate(press_starts):
+
+        default_gap = 100
+        bof_gap = ps
+        prev_ps_gap = 100
+        if pi > 0:
+            prev_ps_gap = ps - press_starts[pi-1]
+        prev_gap = min(default_gap, bof_gap, prev_ps_gap)
+
+        eof_gap = len(ldata) - ps
+        next_ps_gap = 100
+        if pi + 1 < len(press_starts):
+            next_ps_gap = press_starts[pi+1] - ps
+        next_gap = min(default_gap, eof_gap, next_ps_gap)
+
+        
+        if press_lengths[pi] > 10: # definitely overtake
+
+            bnodes = [ldata[ps-j][4] for j in range(prev_gap)]
+            communities, modularity = run_louvain(bnodes)
+            bcomms.append(communities)
+            bmod.append(modularity)
+            acomms.append([])
+            amod.append(-1) # code for "not calculated"
+
+        elif press_lengths[pi] < 9: # definitely oncoming
+
+            anodes = [ldata[ps+j][4] for j in range(next_gap)]
+            communities, modularity = run_louvain(anodes)
+            bcomms.append([])
+            bmod.append(-1) # code for "not calculated"
+            acomms.append(communities)
+            amod.append(modularity)
+
+        else:
+            print("ambiguous press length:", press_lengths[pi], flush=True)
+
+    return (bcomms, acomms, bmod, amod)
+
+def dispersion_score(L):
+    """
+    Input is a list L of N lateral distance values.
+
+    Output is a score (out of N) of pts v = (i, l(i)) such that
+    there's a ball B(v, rx, ry) with radiuses rx and ry around v such
+    that no other u in L is inside B.
+    """
+
+    disp_score = 0
+    rx = 5
+    ry = 30
+    
+    for i, li in enumerate(L):
+        inside_ball_count = 0
+        for j, lj in enumerate(L):
+            # only care about distinct pts
+            if i != j:
+                if abs(i-j) > rx or abs(li - lj) > ry:
+                    continue
+                else:
+                    inside_ball_count += 1
+        if inside_ball_count < 4:
+            disp_score += 1
+
+    return disp_score
+
+                
